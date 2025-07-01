51a6ef569f295157efe66bc23c1e30a661bab821
PORT-GIT <margaretkairu21@gmail.com>
2025-07-01 23:20:33 +0300

    I have created models for the users and services app and migrated them to the Postgresql database

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django. contrib.auth.models import AbstractUser
 
 # Create your models here.
+class customUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('ADMIN', 'Admin'),
+        ('EMPLOYEE', 'Employee'),
+        ('CUSTOMER', 'Customer'),
+    )
+    username = None
+    phoneNumber = models.CharField(max_length=20, null=False)
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='CUSTOMER', null=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return self.email
+
+
+class adminProfile(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user = models.OneToOneField(customUser, on_delete=models.CASCADE, related_name='admin_profile')
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+
+class employeeProfile(models.Model):
+    EMPLOYEEWORK_STATUS = (
+        ('FREE', 'Free'),
+        ('BUSY', 'Busy'),
+        ('AT BREAK', 'At break'),
+    )
+
+    id = models.AutoField(primary_key=True)#this will enable the auto-increment factor
+    user = models.OneToOneField(customUser, on_delete=models.CASCADE, related_name='employee_profile')
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    dateOfBirth = models.DateField(max_length=20)
+    skills = models.CharField (max_length=100)
+    serviceOffered = models.ManyToManyField('services.services')#this imports the services model from the services
+    workStatus = models.CharField(max_length=20, choices=EMPLOYEEWORK_STATUS, default='FREE')
+    employmentDate = models.DateField(null=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    #The value set by auto_now will be updated on every call to the .save() method of the model instance.
+
+class customerProfile(models.Model):
+    id = models.AutoField(primary_key=True)#this will enable the auto-increment factor
+    user = models.OneToOneField(customUser, on_delete=models.CASCADE, related_name='customer_profile')
+    firstName = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    dateOfBirth = models.DateField(max_length=20)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True) 
+    #The value set by auto_now will be updated on every call to the .save() method of the model instance.
