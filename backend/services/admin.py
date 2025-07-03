8a37977e9f1df97dddb076af208e5db652a0963e
PORT-GIT <margaretkairu21@gmail.com>
2025-07-03 05:42:42 +0300

    i have registered the models in admin.py

--- backend/services/admin.py
+++ backend/services/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import services, salonAppointment, servicesGiven
 
 # Register your models here.
+admin.site.register(services)
+admin.site.register (salonAppointment)
+admin.site.register (servicesGiven)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import customerProfile, employeeProfile, adminProfile, customUser
 
 # Register your models here.
+admin.site.register(customerProfile)
+admin.site.register(employeeProfile)
+admin.site.register(adminProfile)
+
+
+#this is how to custom users are properly registered
+class customUserAdmin(UserAdmin):
+    model = customUser
+    list_display = ('username', 'email', 'phoneNumber')
+    list_filter = ('role',)
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('role', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'role', 'is_staff')}
+        ),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('username',)
+
+admin.site.register(customUser, customUserAdmin)
+
