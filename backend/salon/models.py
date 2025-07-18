a137769965af6d75208798f8d40a6c7f8c62f7cb
PORT-GIT <margaretkairu21@gmail.com>
2025-07-18 08:23:12 +0300

    i have created new user roles that inherit the User model

--- backend/salon/models.py
+++ backend/salon/models.py
@@ -1,62 +1,62 @@
-# from django.db import models
+from django.db import models
+from users.models import CustomerProfile, EmployeeProfile
+# Create your models here.
+class service(models.Model):
+    id = models.AutoField(primary_key=True)#this will enable the auto-increment factor
+    category = models.CharField(null=False, max_length=25)
+    service_name = models.CharField(null=False, max_length=25,unique=True)
+    price = models.IntegerField(null=False)
+    description = models.TextField(null=False, max_length=400)
+    durationOfService = models.CharField(null=False, max_length=20)
+    createdAt = models.DateField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
 
-# # Create your models here.
-# class service(models.Model):
-#     id = models.AutoField(primary_key=True)#this will enable the auto-increment factor
-#     category = models.CharField(null=False, max_length=25)
-#     service_name = models.CharField(null=False, max_length=25,unique=True)
-#     price = models.IntegerField(null=False)
-#     description = models.TextField(null=False, max_length=400)
-#     durationOfService = models.CharField(null=False, max_length=20)
-#     createdAt = models.DateField(auto_now_add=True)
-#     updatedAt = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name_plural = 'services'
 
-#     class Meta:
-#         verbose_name_plural = 'services'
+    def __str__(self):
+        return self.service_name
 
-#     def __str__(self):
-#         return self.service_name
+class salonAppointment(models.Model):
+    APPOINTMENT_STATUS = (
+        ('PENDING', 'Pending'),
+        ('APPROVED', 'Approved'),
+        ('REJECTED', 'Rejected'),
+    )
 
-# class salonAppointment(models.Model):
-#     APPOINTMENT_STATUS = (
-#         ('PENDING', 'Pending'),
-#         ('APPROVED', 'Approved'),
-#         ('REJECTED', 'Rejected'),
-#     )
-
-#     id = models.AutoField(primary_key=True)#this will enable the auto-increment factor
-#     customerId = models.ForeignKey('users.customerProfile', on_delete=models.CASCADE)
-#     services = models.ManyToManyField(service, related_name="salon_appointment")
-#     employeeId = models.ForeignKey('users.employeeProfile', on_delete=models.CASCADE)
-#     scheduleDay = models.DateField()
-#     appointmentStatus = models.CharField(max_length=20, choices=APPOINTMENT_STATUS, default='PENDING')
-#     startTime = models.TimeField()
-#     endTime = models.TimeField()
-#     createdAt = models.DateTimeField(auto_now_add=True)
-#     updatedAt = models.DateTimeField(auto_now=True)
+    id = models.AutoField(primary_key=True)#this will enable the auto-increment factor
+    customerId = models.ForeignKey('users.CustomerProfile', on_delete=models.CASCADE)
+    services = models.ManyToManyField(service, related_name="salon_appointment")
+    employeeId = models.ForeignKey('users.EmployeeProfile', on_delete=models.CASCADE)
+    scheduleDay = models.DateField()
+    appointmentStatus = models.CharField(max_length=20, choices=APPOINTMENT_STATUS, default='PENDING')
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
 
     
-#     def __str__(self):
-#      return f"{self.id} - {self.customerId} - {self.employeeId} - {self.scheduleDay}"
+    def __str__(self):
+     return f"{self.id} - {self.customerId} - {self.employeeId} - {self.scheduleDay}"
 
-# class servicesGiven(models.Model):
-#     RATINGS = (
-#     ('1','1'),
-#     ('2', '2'),
-#     ('3', '3'),
-#     ('4', '4'),
-#     ('5', '5'),
-#     )
+class servicesGiven(models.Model):
+    RATINGS = (
+    ('1','1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    )
 
-#     id = models.AutoField(primary_key=True)#this will enable the auto-increment factor
-#     salonAppointmentId = models.ForeignKey(salonAppointment, on_delete=models.SET_NULL, null=True, to_field='id')#sometimes a client cannot have an appointment  
-#     servicesId = models.ForeignKey(service, to_field='id', on_delete=models.CASCADE)
-#     customerId = models.ForeignKey('users.customerProfile',  on_delete=models.CASCADE, to_field='id')
-#     employeeId = models.ForeignKey('users.employeeProfile',  on_delete=models.CASCADE, to_field='id')
-#     customerRating = models.IntegerField(choices=RATINGS)
-#     customerComment = models.TextField(null=False, max_length=200)
-#     employeeRating = models.IntegerField(choices=RATINGS)
-#     employeeComment = models.TextField(null=False, max_length=200)
+    id = models.AutoField(primary_key=True)#this will enable the auto-increment factor
+    salonAppointmentId = models.ForeignKey(salonAppointment, on_delete=models.SET_NULL, null=True, to_field='id')#sometimes a client cannot have an appointment  
+    servicesId = models.ForeignKey(service, to_field='id', on_delete=models.CASCADE)
+    customerId = models.ForeignKey(CustomerProfile,  on_delete=models.CASCADE, to_field='id')
+    employeeId = models.ForeignKey(EmployeeProfile,  on_delete=models.CASCADE, to_field='id')
+    customerRating = models.IntegerField(choices=RATINGS)
+    customerComment = models.TextField(null=False, max_length=200)
+    employeeRating = models.IntegerField(choices=RATINGS)
+    employeeComment = models.TextField(null=False, max_length=200)
 
-#     def __str__(self):
-#         return self.salonAppointmentId
+    def __str__(self):
+        return self.salonAppointmentId
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,4 +1,41 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User
 
+
+class AdminProfile(models.Model):
+    user_profile = models.OneToOneField(User, on_delete=models.CASCADE, related_name='admin_profile')
+    phone_number = models.CharField(null=False, max_length=25)
+
+    # def __str__(self):
+    #     return self.username 
+
+class EmployeeProfile(models.Model):
+
+    WORK_STATUS = (
+        ('FREE', 'Free'),
+        ('BUSY', 'Busy'),
+        ('BREAK', 'At break')
+    )
+
+    id = models.AutoField(primary_key=True)
+    user_profile = models.OneToOneField(User, on_delete=models.CASCADE, related_name='employee_profile')
+    phone_number = models.CharField(null=False, max_length=25)
+    date_of_birth = models.DateField(null=False, blank=False)
+    services_to_offer = models.ManyToManyField('salon.service', verbose_name=('services offered'), related_name="employee_profile")
+    work_status = models.CharField(choices=WORK_STATUS, max_length=20, default='FREE')
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # def __str__(self):
+    #     return str(self.id)+"    "+ self.user.first_name+"    "+self.user.last_name
+    # # this means that the names of the employee will be shown in the UI and admin
+
+
+class CustomerProfile(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_profile = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer_profile')
+    phone_number = models.CharField(null=False, max_length=25)
+    date_of_birth = models.DateField(null=False, blank=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # def __str__(self):
+    #     return self.user.first_name +"    "+ self.user.last_name
